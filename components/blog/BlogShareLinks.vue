41d12688387140c068e3f983039553bd721abc27
Filip Hric <filip@filiphric.com>
2022-12-30 01:06:55 +0100

    test social links

---
+++ components/blog/BlogShareLinks.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <a :href="`https://twitter.com/intent/tweet?url=${tweetText}`">Share on social networks</a>
+
+    <hr class="mb-14 mt-3 w-full border-gray-400 ">
+  </div>
+</template>
+<script setup lang="ts">
+
+const route = useRoute()
+const tweetText = route
+
+// const tweetText = ${doc.slug}&text=${encodeURI(`Hey! I just read \"${doc.title}\" by @filip_hric`)}
+</script>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -10,10 +10,13 @@
           :reading-time="doc.readingTime.text"
         />
         <div class="grid max-w-2xl grid-cols-1 gap-14 lg:max-w-none lg:grid-cols-7">
-          <ContentRenderer :value="doc" class="col-span-5 text-base font-light md:text-lg xl:max-w-4xl" />
+          <div class="col-span-5">
+            <ContentRenderer :value="doc" class=" text-base font-light md:text-lg xl:max-w-4xl" />
+            <BlogShareLinks :blog-info="doc" />
+          </div>
           <div class="col-span-2 mt-5 hidden lg:block">
             <h2 class="text-2xl font-bold">
-              Sidebar content
+              Upcoming workshops
             </h2>
           </div>
         </div>
