f934e51a347db1d221f32e34301fe414bfee73a4
Filip Hric <filip@filiphric.com>
2023-03-01 22:59:48 +0100

    remove registration for workshop if it is too late to register

--- components/Workshops/RegisterForm.vue
+++ components/Workshops/RegisterForm.vue
@@ -1,48 +1,55 @@
 <template>
-  <div class="w-full px-4">
-    <div>
-      <div class="mb-3  text-xl font-extrabold">
-        Please fill in the attendee info:
+  <div>
+    <div v-if="shouldShowRegistration" class="w-full px-4">
+      <div>
+        <div class="mb-3  text-xl font-extrabold">
+          Please fill in the attendee info:
+        </div>
+        <label for="attendeeFirstName" class="block">First name</label>
+        <input
+          v-model="attendeeFirstName"
+          type="text"
+          name="attendeeFirstName"
+          class="mt-1 h-10 w-full rounded-xl bg-white px-5 text-lg shadow-block-tangerine outline-none transition-all dark:rounded-md dark:bg-black-dark dark:shadow-block-none dark:focus:bg-black-lighter"
+        >
+        <label for="attendeeLastName" class="mt-4 block">Last name</label>
+        <input
+          v-model="attendeeLastName"
+          type="text"
+          name="attendeeLastName"
+          class="mt-1 h-10 w-full rounded-xl bg-white px-5 text-lg shadow-block-tangerine outline-none transition-all dark:rounded-md dark:bg-black-dark dark:shadow-block-none dark:focus:bg-black-lighter"
+        >
+        <label for="attendeeEmail" class="mt-4 block">Email</label>
+        <input
+          v-model="attendeeEmail"
+          class="mt-1 h-10 w-full rounded-xl bg-white px-5 text-lg shadow-block-tangerine outline-none transition-all dark:rounded-md dark:bg-black-dark dark:shadow-block-none dark:focus:bg-black-lighter"
+        >
       </div>
-      <label for="attendeeFirstName" class="block">First name</label>
-      <input
-        v-model="attendeeFirstName"
-        type="text"
-        name="attendeeFirstName"
-        class="mt-1 h-10 w-full rounded-xl bg-white px-5 text-lg shadow-block-tangerine outline-none transition-all dark:rounded-md dark:bg-black-dark dark:shadow-block-none dark:focus:bg-black-lighter"
-      >
-      <label for="attendeeLastName" class="mt-4 block">Last name</label>
-      <input
-        v-model="attendeeLastName"
-        type="text"
-        name="attendeeLastName"
-        class="mt-1 h-10 w-full rounded-xl bg-white px-5 text-lg shadow-block-tangerine outline-none transition-all dark:rounded-md dark:bg-black-dark dark:shadow-block-none dark:focus:bg-black-lighter"
-      >
-      <label for="attendeeEmail" class="mt-4 block">Email</label>
-      <input
-        v-model="attendeeEmail"
-        class="mt-1 h-10 w-full rounded-xl bg-white px-5 text-lg shadow-block-tangerine outline-none transition-all dark:rounded-md dark:bg-black-dark dark:shadow-block-none dark:focus:bg-black-lighter"
-      >
+      <div>
+        <p
+          class="mt-5 text-sm text-punch"
+          :class="errors.length ? 'opacity-100' : 'opacity-0'"
+        >
+          {{ errorMessage }}
+        </p>
+      </div>
+      <div id="registration" class="mt-1">
+        <button class="mt-5 w-full rounded-lg bg-white px-7 py-3 text-xl font-black uppercase shadow-block-lime transition-all hover:-translate-y-1 hover:-translate-x-1 hover:shadow-block-hover-lime dark:bg-black-light dark:shadow-block-dark-lime dark:duration-300 dark:hover:translate-x-0 dark:hover:translate-y-0 dark:hover:bg-black-lighter dark:hover:shadow-block-dark-hover-lime" @click="pay(); useTrackEvent('PayButton - ' + info?.title)">
+          Save your spot in upcoming {{ info?.title }}
+        </button>
+        <span class="mt-5 block text-center text-sm text-gray-500">You will be redirected to <IconStripe class="inline-block h-5 pb-0.5" /> checkout page</span>
+      </div>
     </div>
-    <div>
-      <p
-        class="mt-5 text-sm text-punch"
-        :class="errors.length ? 'opacity-100' : 'opacity-0'"
-      >
-        {{ errorMessage }}
-      </p>
-    </div>
-    <div id="registration" class="mt-1">
-      <button class="mt-5 w-full rounded-lg bg-white px-7 py-3 text-xl font-black uppercase shadow-block-lime transition-all hover:-translate-y-1 hover:-translate-x-1 hover:shadow-block-hover-lime dark:bg-black-light dark:shadow-block-dark-lime dark:duration-300 dark:hover:translate-x-0 dark:hover:translate-y-0 dark:hover:bg-black-lighter dark:hover:shadow-block-dark-hover-lime" @click="pay(); useTrackEvent('PayButton - ' + info?.title)">
-        Save your spot in upcoming {{ info?.title }}
-      </button>
-      <span class="mt-5 block text-center text-sm text-gray-500">You will be redirected to <IconStripe class="inline-block h-5 pb-0.5" /> checkout page</span>
+    <div v-else class="grid h-full place-items-center bg-ivory-dark p-7 text-xl font-semibold">
+      <div>Registration to the workshop is currently closed. A new date will be released soon.</div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+
 import { loadStripe } from '@stripe/stripe-js'
 import { Ref } from 'vue'
+import { isGreaterThanToday } from '@/helpers/isGreaterThanToday'
 const config = useRuntimeConfig()
 const stripe = await loadStripe(config.public.stripeApiKey)
 
@@ -55,12 +62,17 @@ const errors: Ref<string[]> = ref([])
 const errorAttendeeFirstName = ref(false)
 const errorAttendeeLastName = ref(false)
 const errorAttendeeEmail = ref(false)
+const shouldShowRegistration = ref(false)
 
 const props = defineProps({
   info: {
     type: Object,
     default: undefined
   }
+})
+
+onMounted(() => {
+  shouldShowRegistration.value = isGreaterThanToday(props.info?.startDate)
 })
 
 function requiredFields (errors: string[]) {
