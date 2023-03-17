a078bbee722cc66b1618bcb8208145b8c2c612a4
Filip Hric <filip@filiphric.com>
2023-03-17 23:09:43 +0100

    try 3

--- components/Home/HomeHero.vue
+++ components/Home/HomeHero.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="place-self-center self-center p-7">
       <div :class="styleClasses" class="overflow-hidden rounded-3xl bg-white md:w-full">
-        <img src="/_nuxt/static/filip_lap2mw.png" alt="Filip Hric">
+        <img src="/static/filip_lap2mw.png" alt="Filip Hric">
       </div>
     </div>
   </div>
