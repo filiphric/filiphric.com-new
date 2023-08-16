9dde4f6845a72b8363aa545017c5795f5020a7cd
Filip Hric <filip@filiphric.com>
2023-08-16 10:14:03 +0200

    fix package

--- components/content/ProseImg.vue
+++ components/content/ProseImg.vue
@@ -1,9 +1,9 @@
 <template>
-  <video
+  <CldVideoPlayer
     v-if="isVideo(src)"
-    autoplay
-    loop
-    :src="data + '/' + src"
+    width="full"
+    height="auto"
+    :src="src"
   />
   <Image
     v-else
@@ -12,7 +12,6 @@
     :width="width"
     :height="height"
     :class="customClass"
-    :quality="quality"
     class="mb-14 mt-10 block"
   />
 </template>
@@ -29,15 +28,11 @@ defineProps({
   },
   width: {
     type: [String, Number],
-    default: 900
+    default: 'full'
   },
   height: {
     type: [String, Number],
-    default: undefined
-  },
-  quality: {
-    type: [String, Number],
-    default: 60
+    default: 'auto'
   },
   customClass: {
     type: String,
@@ -49,11 +44,5 @@ const isVideo = (src: string) => {
   const videoExtensions = ['.mp4', '.avi', '.mov']
   return videoExtensions.some(ext => src.endsWith(ext))
 }
-
-// @ts-ignore
-const { data } = await useAsyncData('cloudinaryBaseUrl', ({ $img }) => {
-  // path to cloudinary baseURL
-  return $img.options.providers.cloudinary.defaults.baseURL.replace('image', 'video')
-})
 
 </script>
