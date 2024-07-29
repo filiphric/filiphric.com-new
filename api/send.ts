68750d288ae4f1f10dd931dc7de7a7fad7caffe7
Filip Hric <filip@filiphric.sk>
2024-07-29 20:07:39 +0200

    update email webhook

--- api/send.ts
+++ api/send.ts
@@ -6,10 +6,10 @@ export default async (req: any, res: any) => {
   const { body } = req
   const data = await resend.emails.send({
     from: 'Filip Hric <filip@filiphric.com>',
-    to: [body.attendeeEmail],
+    to: [body.data.object.customer_details.email],
     cc: ['filip@filiphric.com'],
     subject: 'Cypress core workshop - confirmation',
-    html: `Hello ${body.attendeeFirstName}!
+    html: `Hey!
     Thanks for your registration to Cypress core workshop, I hope you are as excited as I am 🙂
 
 You can expect an instructions email three days before the workshop, where I’ll tell you more about how to connect and what you need to do in preparation.
