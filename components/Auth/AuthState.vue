b742fba17e7c2f69c972563504089a376e09ff43
Filip Hric <filip@filiphric.sk>
2025-01-31 20:29:29 +0100

    move auth to client only and fix css on player

--- components/Auth/AuthState.vue
+++ components/Auth/AuthState.vue
@@ -1,72 +1,74 @@
 <template>
-  <div 
-    class="relative z-30"
-    @mouseenter="isOpen = true"
-    @mouseleave="isOpen = false"
-  >
-    <template v-if="user">
-      <button class="flex items-center w-8 h-8">
-        <img 
-          v-if="profile?.avatar_url"
-          :src="profile.avatar_url" 
-          :alt="profile?.full_name"
-          class="w-8 h-8 rounded-full block"
-        >
-        <IconUser 
-          v-else
-          class="w-8 h-8 block" 
-        />
-      </button>
+  <ClientOnly>
+    <div 
+      class="relative z-30"
+      @mouseenter="isOpen = true"
+      @mouseleave="isOpen = false"
+    >
+      <template v-if="user">
+        <button class="flex items-center w-8 h-8">
+          <img 
+            v-if="profile?.avatar_url"
+            :src="profile.avatar_url" 
+            :alt="profile?.full_name"
+            class="w-8 h-8 rounded-full block"
+          >
+          <IconUser 
+            v-else
+            class="w-8 h-8 block" 
+          />
+        </button>
 
-      <!-- Dropdown Menu -->
-      <div 
-        v-if="isOpen"
-        class="absolute right-0 mt-2 w-48 bg-white dark:bg-black-light shadow-block-lime dark:shadow-block-dark-lime z-50 border-2 border-black"
-      >
-        <div class="absolute h-4 w-full -top-4"></div>
-        <div class="py-2">
-          <NuxtLink 
-            to="/profile"
-            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
-          >
-            Profile
-          </NuxtLink>
-          <NuxtLink 
-            to="/profile?tab=courses"
-            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
-          >
-            Your Courses
-          </NuxtLink>
-          <NuxtLink 
-            to="/profile?tab=membership"
-            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
-          >
-            Membership
-          </NuxtLink>
-          <NuxtLink 
-            to="/profile?tab=certificates"
-            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
-          >
-            Your Certificates
-          </NuxtLink>
-          <button 
-            @click="handleLogout"
-            class="block w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
-          >
-            Logout
-          </button>
+        <!-- Dropdown Menu -->
+        <div 
+          v-if="isOpen"
+          class="absolute right-0 mt-2 w-48 bg-white dark:bg-black-light shadow-block-lime dark:shadow-block-dark-lime z-50 border-2 border-black"
+        >
+          <div class="absolute h-4 w-full -top-4"></div>
+          <div class="py-2">
+            <NuxtLink 
+              to="/profile"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
+            >
+              Profile
+            </NuxtLink>
+            <NuxtLink 
+              to="/profile?tab=courses"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
+            >
+              Your Courses
+            </NuxtLink>
+            <NuxtLink 
+              to="/profile?tab=membership"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
+            >
+              Membership
+            </NuxtLink>
+            <NuxtLink 
+              to="/profile?tab=certificates"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
+            >
+              Your Certificates
+            </NuxtLink>
+            <button 
+              @click="handleLogout"
+              class="block w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-black-lighter"
+            >
+              Logout
+            </button>
+          </div>
         </div>
-      </div>
-    </template>
-    <template v-else>
-      <NuxtLink 
-        to="/login"
-        class="prettyLink"
-      >
-        Login
-      </NuxtLink>
-    </template>
-  </div>
+      </template>
+      <template v-else>
+        <NuxtLink 
+          to="/login"
+          class="prettyLink"
+        >
+          Login
+        </NuxtLink>
+      </template>
+    </div>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
