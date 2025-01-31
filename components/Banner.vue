948c4cab35cbc0760b678f451255d06670a3c8d0
Filip Hric <filip@filiphric.sk>
2025-01-31 16:24:00 +0100

    improve banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,16 +1,27 @@
 <script setup>
 const timeLeft = ref('')
 const isOfferExpired = ref(false)
+const isOfferStarted = ref(false)
 
 const calculateTimeLeft = () => {
-  const endDate = new Date('2025-02-09T23:59:59+01:00')
+  const startDate = new Date('2025-02-03T00:00:00+01:00') // CET timezone
+  const endDate = new Date('2025-02-09T23:59:59+01:00') // CET timezone
   const now = new Date()
-  const difference = endDate - now
 
-  if (difference <= 0) {
+  // Check if offer hasn't started yet
+  if (now < startDate) {
+    isOfferStarted.value = false
+    return
+  }
+
+  // Check if offer has ended
+  if (now > endDate) {
     isOfferExpired.value = true
     return
   }
+
+  isOfferStarted.value = true
+  const difference = endDate - now
 
   const days = Math.floor(difference / (1000 * 60 * 60 * 24))
   const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -34,7 +45,7 @@ onUnmounted(() => {
 
 <template>
   <NuxtLink to="/course/99-cypress-tips">
-    <div v-if="!isOfferExpired" class="bg-tangerine dark:bg-gray-800 p-4 sticky top-0 z-40">
+    <div v-if="isOfferStarted && !isOfferExpired" class="bg-tangerine dark:bg-gray-800 p-4 sticky top-0 z-40">
       <div to="/course/99-cypress-tips" class="text-lg md:text-xl lg:text-xl font-extrabold">
         <p class="text-center">
           Check out my newly released course <span class="text-white">99 Cypress.io Tips</span> for just 49 € 
